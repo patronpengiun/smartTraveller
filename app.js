deb9acb8453b8fc110557c67c57ff690ae90da9f
patronpenguin <xh5277@gmail.com>
2015-04-02 15:36:47 -0400

    changed port

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var routes = require('./routes/index')(passport);
 app.use('/', routes);
 
 
-var server = app.listen(3000, function() {
+var server = app.listen((process.env.PORT || 5000), function() {
 	var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
