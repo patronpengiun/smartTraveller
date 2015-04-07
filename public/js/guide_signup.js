51b72a030d0f9c857e0f5550338c78de8fa778ee
hj690 <hj690@nyu.edu>
2015-04-07 18:46:38 -0400

    almost finished styling of guide sign up page

--- public/js/guide_signup.js
+++ public/js/guide_signup.js
@@ -1,42 +1,25 @@
 $(function() {
 	$('.btn-next').click(function() {
-		var cur_step = $(this).data('currentStep');
-		if (!$('#signup-form').parsley().validate('step' + cur_step)) {
+		//if(!$('#signup-form').parsley().validate('step1')){
+		//	return;
+		//} else {
+			$('.form-step1').removeClass('show').addClass('hidden');
+			$('.form-step2').removeClass('hidden').addClass('show');
+		//}
+	});	
+
+	$('.btn-previous').click(function() {
+			$('.form-step2').removeClass('show').addClass('hidden');
+			$('.form-step1').removeClass('hidden').addClass('show');
+	});	
+
+	$('.btn-submit').click(function() {
+		if(!$('#signup-form').parsley().validate('step2')){
 			return;
 		} else {
-			if (3 == cur_step) {
-				for(var i = 1; i < 3; i++){
-					if (!$('#signup-form').parsley().validate('step' + i)) {
-						$('#form-step' + 3).removeClass('show').addClass('hidden');
-						$('#form-step' + i).removeClass('hidden').addClass('show');
-						$('#step-' + 3).removeClass('active').addClass('complete');
-						$('#step-' + i).removeClass('disable').addClass('active');
-						$('#signup-form').data('currentStep', i);
-					}
-				}
-				$('#signup-form').submit();
-			} else {
-				$('#form-step' + cur_step).removeClass('show').addClass('hidden');
-				$('#form-step' + (cur_step+1)).removeClass('hidden').addClass('show');
-				$('#step-' + cur_step).removeClass('active').addClass('complete');
-				$('#step-' + (cur_step+1)).removeClass('disable').addClass('active');
-				$('#signup-form').data('currentStep', cur_step + 1);
-			}
+			$('#signup-form').submit();
 		}
-	});
-
-
-	$('.form-step').click(function() {
-		if (!$(this).hasClass('complete')){
-			return;
-		}
-		var next_step = $(this).data('step');
-		var currentStep = $('#signup-form').data('currentStep');
-		$('#form-step' + currentStep).removeClass('show').addClass('hidden');
-		$('#form-step' + next_step).removeClass('hidden').addClass('show');
-		$('#step-' + currentStep).removeClass('active').addClass('complete');
-		$('#step-' + next_step).removeClass('complete').addClass('active');
-		$('#signup-form').data('currentStep', next_step);
+			
 	});
 
 });
