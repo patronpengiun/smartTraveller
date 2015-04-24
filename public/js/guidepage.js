0bcb212e074e7012586b63880b2f62bb617ea7b4
LiangFang <liangfang09@gmail.com>
2015-04-24 01:33:23 -0400

    Add photo view section

--- public/js/guidepage.js
+++ public/js/guidepage.js
@@ -1,20 +1,47 @@
 $(document).ready(function() {
-	var length = parseInt($('#photo-life-count').val());
-	var curr = 0;
+	var length_life = parseInt($('#photo-life-count').val());
+	var curr_life = 0;
 
 	$('#photo_life_0').removeClass("hidden");	
 	
-	$('#photo_life_next_button').click(function() {
-		$('#photo_life_' + curr).addClass("hidden");
-		var next = (curr + 1) % length;
+	$('#photo_life_next_button').click(function(e) {
+		$('#photo_life_' + curr_life).addClass("hidden");
+		var next = (curr_life + 1) % length_life;
 		$('#photo_life_' + next).removeClass("hidden");
-		curr = next;
+		curr_life = next;
+
+		e.preventDefault();	// prevent refresh the page 
 	});
 
-	$('#photo_life_previous_button').click(function() {
-		$('#photo_life_' + curr).addClass("hidden");
-		var previous = (curr - 1 + length) % length;
+	$('#photo_life_previous_button').click(function(e) {
+		$('#photo_life_' + curr_life).addClass("hidden");
+		var previous = (curr_life - 1 + length_life) % length_life;
 		$('#photo_life_' + previous).removeClass("hidden");
-		curr = previous;
+		curr_life = previous;
+
+		e.preventDefault();
+	});
+
+	var length_view = parseInt($('#photo-view-count').val());
+	var curr_view = 0;
+
+	$('#photo_view_0').removeClass("hidden");	
+	
+	$('#photo_view_next_button').click(function(e) {
+		$('#photo_view_' + curr_view).addClass("hidden");
+		var next = (curr_view + 1) % length_view;
+		$('#photo_view_' + next).removeClass("hidden");
+		curr_view = next;
+
+		e.preventDefault();
+	});
+
+	$('#photo_view_previous_button').click(function(e) {
+		$('#photo_view_' + curr_view).addClass("hidden");
+		var previous = (curr_view - 1 + length_view) % length_view;
+		$('#photo_view_' + previous).removeClass("hidden");
+		curr_view = previous;
+
+		e.preventDefault();
 	});
 });
