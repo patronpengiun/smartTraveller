2383e0b829402370831d666c77d7a7b722ba9d0e
patronpenguin <xh5277@gmail.com>
2015-05-04 15:30:43 -0400

    complete dashboard request page

--- models/review.js
+++ models/review.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
  
 module.exports = mongoose.model('Review',{
+	request_id: String,
     reviewer_id: String,
     reviewer_name: String,
     reviewee_id: String,
