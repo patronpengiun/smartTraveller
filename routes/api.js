0d9dc9dc2fc710b8ebf258c0fee177ed600db744
patronpenguin <xh5277@gmail.com>
2015-08-08 13:48:19 -0700

    fix bug

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var api_router = express.Router();
 
-var Guide = require('../models/guide');
-
 module.exports = function() {
 	api_router.get('/guide/:guide_id', function(req, res) {
 		Guide.find({_id:req.params.guide_id}, function(err, guides) {
